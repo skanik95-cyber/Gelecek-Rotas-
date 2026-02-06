97f62fbec6da21632459b4ecc574dea95fc3a3ce
gelecek-rotasi <skanik95@gmail.com>
2026-02-06 16:33:32 +0300

    Update Firebase configuration and initialization
    Replaced old Firebase configuration with new settings and updated initialization code.

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,23 @@
-const firebaseConfig = {
-  apiKey: "BURAYA_API_KEY",
-  authDomain: "BURAYA.firebaseapp.com",
-  projectId: "BURAYA",
-  storageBucket: "BURAYA.appspot.com",
-  messagingSenderId: "BURAYA",
-  appId: "BURAYA"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-firebase.initializeApp(firebaseConfig);
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyD-RKrwpZ3jTsZaoRtHjg1SYlZLbI-Hfuw",
+    authDomain: "gelecek-rotasi-web.firebaseapp.com",
+    projectId: "gelecek-rotasi-web",
+    storageBucket: "gelecek-rotasi-web.firebasestorage.app",
+    messagingSenderId: "1035439856136",
+    appId: "1:1035439856136:web:6de6c461d03304daea0fbd",
+    measurementId: "G-2WQNX3X9Z8"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
