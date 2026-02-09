cfd1cb194df13a83dc0dd68533d8651d03f75f75
gelecek-rotasi <skanik95@gmail.com>
2026-02-09 14:57:53 +0300

    Route auth success to action selection

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "BURAYA_API_KEY",
+  authDomain: "BURAYA.firebaseapp.com",
+  projectId: "BURAYA",
+  storageBucket: "BURAYA.appspot.com",
+  messagingSenderId: "BURAYA",
+  appId: "BURAYA"
+};
+
+firebase.initializeApp(firebaseConfig);
